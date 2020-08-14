f1d70e284cdd04f9c88c579cd10f46e4d1cb13df
unknown <superfly09@daum.net>
2020-08-14 17:13:06 +0900

    chore: package.json engine=>engins로 수정

---
+++ client/src/components/Config.js
@@ -0,0 +1,4 @@
+//API KEY
+export const API_KEY ='055121a491d68e113964c957a2913342';
+export const API_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMAGE_BASE_URL='https://image.tmdb.org/t/p/'
