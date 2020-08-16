e961ef576917e48ab5612beb9919061bf2356fe3
unknown <superfly09@daum.net>
2020-08-16 13:21:12 +0900

    fix: searchedPage style추가

--- client/src/components/SearchedPage/SearchedPage.js
+++ client/src/components/SearchedPage/SearchedPage.js
@@ -1,13 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { Fragment } from 'react'
+import {useSelector} from 'react-redux';
+import GridCard from '../views/Util/GridCard';
+import { IMAGE_BASE_URL } from '../Config';
+import './SearchedPage.css';
 
 function SearchedPage(props) {
-    useEffect(()=>{
+    const movie = useSelector(state=>state.movie);
+    const {movieResult} = movie;
+    const resultLength = movieResult.length;
+    console.log('movie:',movieResult);
 
-    },[]);
+    const renderMovie = movieResult.map((movieInfo,index)=>(
+        <Fragment key={index}>
+            <GridCard
+                searchMovie
+                image={`${IMAGE_BASE_URL}w500${movieInfo.poster_path}`}
+                title={movieInfo.title}
+                release_date={movieInfo.release_date}
+                vote_average={movieInfo.vote_average}
+                id={movieInfo.id}
+                overview={movieInfo.overview}
+                resultLength={resultLength}
+            />
+        </Fragment>
+    ))
 
     return (
-        <div>
-            SearchEd
+        <div className='search_movie_container'>
+            {renderMovie}
         </div>
     )
 }
--- client/src/components/views/Util/GridCard.js
+++ client/src/components/views/Util/GridCard.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Col } from 'antd'
 import './GridCard.css';
 
 function GridCard(props) {
-    const {image,title,release_date,vote_average,id} = props;
+    const {image,title,release_date,vote_average,id,overview,resultLength} = props;
     if (props.landingPage) {
         return (
             <Col lg={6} md={8} xs={24}>
@@ -17,6 +17,42 @@ function GridCard(props) {
                     </div>
                 </div>
             </Col>
+        )
+    } else if (props.searchMovie) {
+        return (
+            <Fragment>
+                <Col lg={6} md={6} xs={6}>
+                    <p>Searched Result</p>
+                    <ul className='search_length_bar'>
+                        <li>
+                            <span>Movie</span>
+                            <a>{resultLength}</a>
+                        </li>
+                        <li>
+                            <span>TV Shows</span>
+                            <a>{0}</a>
+                        </li>
+                
+                        <li>
+                            <span>Keywords: </span>
+                            <a>{title}</a>
+                            <a>{release_date}</a>
+                        </li>
+                    </ul>
+                </Col>
+                <Col lg={18} md={18} xs={18}>
+                    <div className='grid_container'>
+                        <a href={`/movie/${id}`}>
+                            <img className='grid_img' src={image} alt={title}></img>
+                        </a>
+                        <div className='movie_info'>
+                            <span className='movie_info_title'>{title}</span>
+                            <span className='movie_info_release_date'>{release_date}</span>
+                            <span className='movie_info_overview'>{overview}</span>
+                        </div>
+                    </div>
+                </Col>
+            </Fragment>
         )
     } else {
         return (
