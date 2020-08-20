a5199f59aa8f95504ae82cca3d2fa980cfad00ba
unknown <superfly09@daum.net>
2020-08-20 15:22:58 +0900

    chore: fix untracted files

--- client/src/components/Config.js
+++
@@ -1,5 +0,0 @@
-//API KEY
-export const API_KEY ='055121a491d68e113964c957a2913342';
-export const API_URL = 'https://api.themoviedb.org/3/movie/';
-export const SEARCH_URL='https://api.themoviedb.org/3/search/movie';
-export const IMAGE_BASE_URL='https://image.tmdb.org/t/p/'
